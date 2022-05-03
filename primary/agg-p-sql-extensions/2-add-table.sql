810b946cb852e893c1b5e293ae892ce9de3737b4
kjellmlarsson <96128562+kjellmlarsson@users.noreply.github.com>
2022-05-03 16:03:18 +0200

    Update 2-add-table.sql

--- primary/agg-p-sql-extensions/2-add-table.sql
+++ primary/agg-p-sql-extensions/2-add-table.sql
@@ -1,7 +1,2 @@
- -- create a custom table now
-  create table mydb.mytable persistent
-  (
-  col1 incr primary key,
-  col2 varchar(255)
-  );
-  go
+ALTER TABLE alerts ADD new_column VARCHAR(50);
+GO
